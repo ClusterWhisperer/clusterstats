5063da6825f3e4874155f3bef16c8a2d067b77fd
ClusterWhisperer <clusterwhisperer@gmail.com>
2017-08-10 16:58:41 -0700

    Pushing setup.py with uri

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    # url='https://github.com/pypa/sampleproject',
+    url='https://github.com/ClusterWhisperer/clusterstats',
 
     # Author details
     author='Kumar Kandasami',
